116240ac07d6110e2f479998084ff5b3ac3cbdaa
Moojig Chimid <me@moojig.dev>
2022-09-10 13:12:15 -0400

    env

--- config/Connection.js
+++ config/Connection.js
@@ -1,21 +1,23 @@
-const mysql = require('mysql2/promise');
+const mysql = require("mysql2/promise");
+require("./env");
 
 class Connection {
-    constructor(config) {
-        const { DATABASE, HOST, USER, PASSWORD } = config
-        this.database = DATABASE
-        this.host = HOST
-        this.user = USER
-        this.password = PASSWORD
-    }
-    async dbConnect() {
-        return await mysql.createPool({
-            database: this.database,
-            host: this.host,
-            user: this.user,
-            password: this.password
-        });
-    }
+  constructor(config) {
+    const { DATABASE, DB_HOST, DB_USER, DB_PASSWORD } = config;
+    console.log(`Connected to ${DATABASE} on ${DB_HOST} as ${DB_USER}`);
+    this.database = DATABASE;
+    this.host = DB_HOST;
+    this.user = DB_USER;
+    this.password = DB_PASSWORD;
+  }
+  async dbConnect() {
+    return await mysql.createPool({
+      database: this.database,
+      host: this.host,
+      user: this.user,
+      password: this.password,
+    });
+  }
 }
 
 module.exports = Connection;
